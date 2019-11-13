d9f2b416b71ad1cd76238b36f5aa33a0a7225666
ronaldvenegas1991 <ronald.venegas@corpsistem.com.co>
2019-11-13 10:08:06 -0500

    Aplicación de películas en Agular creada. Clase 234 del curso de Angular completa.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { SearchComponent } from './components/shared/search/search.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
+import { HeaderComponent } from './components/shared/header/header.component';
+import { PeliculaComponent } from './components/pelicula/pelicula.component';
+import { PeliculasComponent } from './components/peliculas/peliculas.component';
+import { PeliculaTarjetaComponent } from './components/pelicula-tarjeta/pelicula-tarjeta.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    SearchComponent,
+    FooterComponent,
+    HeaderComponent,
+    PeliculaComponent,
+    PeliculasComponent,
+    PeliculaTarjetaComponent
   ],
   imports: [
     BrowserModule,
